164ed5e982e588defd06321c5bb2a789ba9bd73c
berrucaz <sev.berrucaz@gmail.com>
2021-04-01 12:58:17 +0200

    Ajout de l'attribut altitude_max à Oiseau avec le mot clé 'Super'

--- Oiseau.py
+++ Oiseau.py
@@ -2,9 +2,11 @@ from Animal import *
 
 class Oiseau(Animal):
     
-    def __init__(self,poids,taille):
-        Animal.__init__(self,poids,taille)
-    
+    def __init__(self,poids,taille,altitude_max):
+        #Animal.__init__(self,poids,taille)
+        super(Oiseau,self).__init__(poids,taille)
+        altitude_max=1000
+
     def se_deplacer(self):
-        print("Mes chers parents,je vole")
-    
+        print("Mes chers parents,je vole")   
+
--- ProgPrincipal.py
+++ ProgPrincipal.py
@@ -2,7 +2,7 @@ from Serpent import *
 from Oiseau import *
 
 longSerpent = Serpent(1.5,1)
-petitOiseau = Oiseau(0.2,0.15)
+petitOiseau = Oiseau(0.2,0.15,1500)
 
 longSerpent.se_deplacer()
 petitOiseau.se_deplacer()
